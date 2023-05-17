6fe06b0bfbe42d9ab3636413230c9fbd6bfea320
keeganpearson <keegan.dpearson@gmail.com>
2023-05-17 21:41:11 +0000

    Updated tests to check for words vertically instead of just horizontally, as well as for empty arrays.

--- test/test_wordsearch.js
+++ test/test_wordsearch.js
@@ -35,4 +35,46 @@ describe("#wordSearch()", function() {
 
     assert.isTrue(result);
   });
+
+  // New test for a word written vertically
+  it("should return true if the word is present vertically", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['U', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['L', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['T', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['H', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['O', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['O', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['D', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'ADULTHOOD')
+
+    assert.isTrue(result);
+  });
+
+  // test for an empty array
+  it("should return false if word matrix is empty array", function() {
+    const result = wordSearch([], 'FRANK')
+
+    assert.isFalse(result);
+  });
+
+  // test for no word present
+  it("should return false if word not present", function() {
+    const result = wordSearch([
+      ['A', 'W', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['B', 'E', 'I', 'N', 'F', 'E', 'L', 'D'],
+      ['C', 'I', 'C', 'F', 'Q', 'U', 'A', 'L'],
+      ['D', 'M', 'J', 'T', 'E', 'V', 'R', 'G'],
+      ['E', 'H', 'C', 'S', 'Y', 'E', 'R', 'L'],
+      ['F', 'F', 'R', 'E', 'N', 'E', 'Y', 'B'],
+      ['G', 'B', 'T', 'W', 'A', 'P', 'A', 'I'],
+      ['H', 'D', 'C', 'A', 'K', 'U', 'A', 'S'],
+      ['I', 'Z', 'K', 'F', 'Q', 'U', 'A', 'L'],
+    ], 'XYZ')
+
+    assert.isFalse(result);
+  });
 });
+
